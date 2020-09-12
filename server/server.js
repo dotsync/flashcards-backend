35c6e660c9a8a48b8651dd5b3f65beac3c11c354
Sergei Liddell <liddell.s@pm.me>
2020-09-12 13:45:51 -0600

    Implements CORS header response from server

--- server/server.js
+++ server/server.js
@@ -8,6 +8,17 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
+// add headers to response
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('cces-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  next();
+})
+
 app.use('/flashcards', flashcardRoutes)
 
 mongoose
