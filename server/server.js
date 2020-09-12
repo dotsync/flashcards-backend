c7ce3acee411817911472b21a747dfce9037b976
Sergei Liddell <liddell.s@pm.me>
2020-09-12 10:48:37 -0600

    Securly connects to server useing hidden keys
    * Connected using express

--- server/server.js
+++ server/server.js
@@ -1,4 +1,21 @@
-// import express
-// set upp middle ware
+const access = require('../access/access')
+
+const express = require('express');
+const bodyParser = require('body-parser');
+// set up middle ware
+const app = express();
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+
+
+
+
+
+
+
 // setup routes
 // connect to mongoose
+app.listen(access.local, () => {
+  console.log(`Express succesfully connected to local port and on standby.`)
+})
