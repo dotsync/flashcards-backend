ff3deeafa6af12953bca433baf0917790a77b3f2
Sergei Liddell <liddell.s@pm.me>
2020-09-12 18:59:06 -0600

    Plans implementation of createFlashcard function

--- server/controllers/flashcardControllers.js
+++ server/controllers/flashcardControllers.js
@@ -1,6 +1,10 @@
+// import mock data
 const mockData = require('../../mock/data-files/dummies')
+
+// import flashcard model
 const Flashcard = require('.././models/flashcardSchema')
 
+// get a list of all flashcards from the db
 const getFlashcards = async (req, res) => {
   try {
     const flashcards = await Flashcard.find();
@@ -10,7 +14,32 @@ const getFlashcards = async (req, res) => {
     console.log(err)
   }
 };
+// create a flashcard and store it on the server
+const createFlashcard = async (req, res) => {
+  // connect to db
+  // create flashcard
+  try {
+    // save flashcard to db
+  } catch (err) {
+    console.log(err)
+  }
+  // close connection to db
+  // respond to client
+}
 
+// export controllers to routes
 module.exports = {
   getFlashcards,
+  createFlashcard,
 }
+/* example data
+{
+  flashcard_id: {type: String},
+  createdAt: {type: String},
+  category: {type: String},
+  subject: {type: String},
+  question: {type: String},
+  answer: {type: String},
+  amountOfFlips: {type: String}
+}
+*/
