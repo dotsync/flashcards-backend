e56c3840c535a0969677d90786b35a999af27af2
Sergei Liddell <liddell.s@pm.me>
2020-09-12 18:14:24 -0600

    Implements getFlashcards function

--- server/controllers/flashcardControllers.js
+++ server/controllers/flashcardControllers.js
@@ -1,10 +1,16 @@
 const mockData = require('../../mock/data-files/dummies')
+const Flashcard = require('.././models/flashcardSchema')
 
-const getFlashcards = (req, res) => {
-  let flashcards = mockData.flashcards
-  res.status(200).send(flashcards);
-}
+const getFlashcards = async (req, res) => {
+  try {
+    const flashcards = await Flashcard.find();
+    res.json(flashcards);
+    res.status('200')
+  } catch (err) {
+    console.log(err)
+  }
+};
 
 module.exports = {
-  getFlashcards
+  getFlashcards,
 }
